9ed1d2aa324ac43d3c9d3536201f44304dbb41ff
Lokeswari-Busam <lokeswaribusam216@gmail.com>
2025-12-31 12:32:36 +0530

    added jwt open calls

--- Backend/API_Layer/middleware/jwt_middleware.py
+++ Backend/API_Layer/middleware/jwt_middleware.py
@@ -8,7 +8,7 @@ class JWTMiddleware(BaseHTTPMiddleware):
     def __init__(self, app):
         super().__init__(app)
         # Add paths you want to skip
-        self.open_endpoints = ["/docs", "/openapi.json", "/redoc", "/offerresponse" ,"/masters/country", "/employee-details"]
+        self.open_endpoints = ["/docs", "/openapi.json", "/redoc", "/offerresponse" ,"/masters/country", "/employee-details", "/employee-details/address", "/identity/country-mapping","/employee-details/identity", "/experience/", "/otp/send","/otp/verifyOtp", "/token-verification/", "/offerletters/offer/"]
 
     async def dispatch(self, request: Request, call_next):
         path = request.url.path
